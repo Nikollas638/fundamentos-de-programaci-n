cc02eb22e7363c4416dd94fc216380e29205140b
Nikollas638 <nikollas638@gmail.com>
2025-03-11 11:58:54 -0500

    Add files via upload

---
+++ taller 1, punto 2.py
@@ -0,0 +1,8 @@
+#Construir un programa que lea un número entero mayor que 2 y devuelva como resultado el número primo de valor más cercano, en este caso menor o igual, al número leído.
+
+numero=(input("ingrese el numero (mayor que 2): "))
+
+if numero <=2:
+    print( (numero), "no es mayor que 2")
+else:
+    
